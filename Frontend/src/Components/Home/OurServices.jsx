5306f72d7c40760b8bb23f1b0b5019dfe8953ce7
JunaidShaikh01 <shaikhjunaid020801@gmail.com>
2024-11-17 13:32:51 +0530

    our services section fixed

--- Frontend/src/Components/Home/OurServices.jsx
+++ Frontend/src/Components/Home/OurServices.jsx
@@ -12,22 +12,26 @@ export default function OurServices() {
 
   return (
     <div
-      className="bg-fixed bg-cover bg-center min-h-screen relative overflow-y-auto "
+      className="bg-fixed bg-cover bg-center min-h-screen relative overflow-scroll"
       style={{ backgroundImage: `url(${AboutHomeBanner1})` }}
     >
-      <div className="absolute inset-0 bg-black opacity-70 z-10 text-white flex flex-col items-center justify-center">
+      {/* Black Overlay */}
+      <div className="absolute inset-0 bg-black opacity-80 z-10"></div>
+
+      {/* Content */}
+      <div className="relative z-20 text-white flex flex-col items-center justify-center px-4 py-10 sm:py-20">
         {/* Title Section */}
         <div
-          className="text-center text-white mb-20 uppercase z-100 underline-offset-1 "
+          className="text-center uppercase mb-10 px-4"
           data-aos="zoom-in"
         >
-          <h1 className="text-4xl sm:text-5xl md:text-6xl z-100 mb-5 font-extrabold">
-            Our <span className="text-[#ce9233]">services</span>
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold mb-5">
+            Our <span className="text-[#ce9233]">Services</span>
           </h1>
-          <p className="text-xl font-bold" style={{ letterSpacing: "0.1em" }}>
+          <p className="text-xl sm:text-2xl font-bold">
             WELDWELL ENGINEERING COMPANY
           </p>
-          <p className="border-b border-[#ce9233] mt-4"></p>
+          <p className="border-b border-[#ce9233] mt-4 w-1/2 mx-auto"></p>
         </div>
 
         {/* Service Items Section */}
@@ -36,11 +40,14 @@ export default function OurServices() {
           data-aos="zoom-in"
         >
           {Array.from({ length: 6 }, (_, index) => (
-            <div key={index} className="flex items-center flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-              <h1 className="text-center text-2xl sm:text-3xl font-bold">
+            <div
+              key={index}
+              className="flex items-center flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/4 text-center"
+            >
+              <h1 className="text-2xl sm:text-3xl font-bold mb-2">
                 ARC <span className="text-[#ce9233]">Welding</span> Rectifier
               </h1>
-              <p className="text-center font-semibold">
+              <p className="font-semibold">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Voluptates, cumque.
               </p>
